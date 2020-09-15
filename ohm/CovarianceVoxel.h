e7bbb18c9c642df5bb8726bd4d11cf7a992747b5
Kazys Stepanas <kazys.stepanas@csiro.au>
2020-09-15 12:34:46 +1000

    Change NDT initialisation to be entirely based on the voxel size
    This makes for better consistency between the various code paths taken to get there.

--- ohm/CovarianceVoxel.h
+++ ohm/CovarianceVoxel.h
@@ -76,12 +76,13 @@ typedef struct CovarianceVoxel_t
 /// The covariance value is initialised to an identity matrix, scaled by the @p covariance_initialisation .
 /// This is to ensure we do not start with a zero matrix, which causes all sorts of mathematical problems.
 /// @param[out] cov The @c CovarianceVoxel to initialse.
-/// @param covariance_initialisation An initialisation value for the covariance matrix diagonal.
-inline __device__ void initialiseCovariance(CovarianceVoxel *cov, float covariance_initialisation)
+/// @param voxel_resolution The voxel resolution, which is used to seed the covariance.
+inline __device__ void initialiseCovariance(CovarianceVoxel *cov, float voxel_resolution)
 {
+  const float covariance_scale_factor = 0.1f;
   // Initialise the square root covariance matrix to a scaled identity matrix.
   cov->trianglar_covariance[0] = cov->trianglar_covariance[2] = cov->trianglar_covariance[5] =
-    covariance_initialisation;
+    covariance_scale_factor * voxel_resolution;
   cov->trianglar_covariance[1] = cov->trianglar_covariance[3] = cov->trianglar_covariance[4] = 0;
 }
 
@@ -203,15 +204,14 @@ inline __device__ covvec3 solveTriangular(const CovarianceVoxel *cov, const covv
 /// @param hit_value The log probably value increase for occupancy on a hit. This must be greater than zero to increase
 /// the voxel occupancy probability.
 /// @param uninitialised_value The @p voxel_value for an uncertain voxel - one which has yet to be observed.
-/// @param covariance_initialisation An initialisation value for the covariance matrix diagonal. A value an order of
-/// magnitude smaller than the voxel resolution is recommended. Must be greater than zero.
+/// @param voxel_resolution Specifies the voxel resolution. Used to initialse the covariance value.
 /// @param reinitialise_threshold @p voxel_value threshold below which the covariance and mean should reset.
 /// @param reinitialise_sample_count The @p point_count required to allow @c reinitialise_threshold to be triggered.
 /// @return True if the covariance value is re-initialised. This should be used as a signal to diregard the current
 ///     @p voxel_mean and @c point_count and restart accumulating those values.
 inline __device__ bool calculateHitWithCovariance(CovarianceVoxel *cov_voxel, float *voxel_value, covvec3 sample,
                                                   covvec3 voxel_mean, unsigned point_count, float hit_value,
-                                                  float uninitialised_value, float covariance_initialisation,
+                                                  float uninitialised_value, float voxel_resolution,
                                                   float reinitialise_threshold, unsigned reinitialise_sample_count)
 {
   const float initial_value = *voxel_value;
@@ -220,7 +220,7 @@ inline __device__ bool calculateHitWithCovariance(CovarianceVoxel *cov_voxel, fl
 
   if (point_count == 0 || initial_value < reinitialise_threshold && point_count >= reinitialise_sample_count)
   {
-    initialiseCovariance(cov_voxel, covariance_initialisation);
+    initialiseCovariance(cov_voxel, voxel_resolution);
     initialised_covariance = true;
     // Reinitialising co-variance. We need to ensure the point count is zero when we reset the covariance to
     // correctly calculate the covariance.
