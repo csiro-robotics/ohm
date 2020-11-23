48066d6ea8104fab038356f20dc5b48dae0deb12
Jason Williams <jason.williams@data61.csiro.au>
2020-11-23 22:57:42 +1000

    Save/load to/from encoded message

--- ohm/Snapshot.cpp
+++ ohm/Snapshot.cpp
@@ -112,6 +112,31 @@ Snapshot::SnapshotNode::SnapshotNode(const glm::i16vec3 &region_key, const Occup
 }
 
 
+Snapshot::SnapshotNode::SnapshotNode(const SnapshotRegion &region, size_t &byte_address,
+                                       uint8_t &bit_address)
+{
+  state_ = static_cast<State>((region.data[byte_address] >> bit_address) & kStateMask);
+  bit_address += kStateBits;
+  if (bit_address == 8)
+  {
+    bit_address = 0;
+    ++byte_address;
+  }
+  if (state_ == kNonLeaf)
+  {
+    children_ = std::make_unique<std::array<SnapshotNode, 8>>();
+    for (auto &ch : *children_)
+    {
+      ch = SnapshotNode(region, byte_address, bit_address);
+    }
+  }
+}
+
+Snapshot::SnapshotNode::SnapshotNode(const SnapshotNode &ss)
+: state_(ss.state_)
+{}
+
+
 Snapshot::SnapshotNode::~SnapshotNode() = default;
 
 
@@ -161,6 +186,16 @@ std::tuple<uint16_t, uint16_t, uint16_t> Snapshot::SnapshotNode::simplify(float 
     delete children_.release();
   }
   else if (voxel_size < 1.0 && std::get<0>(res) > std::get<2>(res) && std::get<1>(res) == 0)
+  {
+    state_ = Snapshot::State::kFree;
+    delete children_.release();
+  }
+  else if (voxel_size < 0.25 && std::get<1>(res) > 0)
+  {
+    state_ = Snapshot::State::kOccupied;
+    delete children_.release();
+  }
+  else if (voxel_size < 0.25 && std::get<0>(res) > 0)
   {
     state_ = Snapshot::State::kFree;
     delete children_.release();
@@ -242,190 +277,62 @@ size_t Snapshot::SnapshotNode::numNode() const
 }
 
 
+SnapshotRegion Snapshot::SnapshotNode::getRegion(double time, const glm::i16vec3 &region_key) const
+{
+  SnapshotRegion region;
+  region.time = time;
+  region.region_key = region_key;
+  region.data.reserve((numNode() >> kStateBits) + 1);
+  uint8_t bits_used_in_final_byte = 0;
+  getRegion(region, bits_used_in_final_byte);
+  return region;
+}
+
+
+void Snapshot::SnapshotNode::getRegion(SnapshotRegion &region, uint8_t &bits_used_in_final_byte) const
+{
+  if (bits_used_in_final_byte == 0 || bits_used_in_final_byte == 8)
+  {
+    region.data.push_back(0);
+    bits_used_in_final_byte = 0;
+  }
+
+  uint8_t &byte = region.data.back();
+  byte = byte | ((state_ & kStateMask) << bits_used_in_final_byte);
+  bits_used_in_final_byte += kStateBits;
+
+  if (state_ == kNonLeaf)
+  {
+    if (!children_)
+    {
+      throw std::runtime_error("Non-leaf node had no children");
+    }
+    for (const auto &ch : *children_)
+    {
+      ch.getRegion(region, bits_used_in_final_byte);
+    }
+  }
+}
+
+
 Snapshot::SnapshotNode &Snapshot::SnapshotNode::operator=(SnapshotNode &&s) = default;
 
 
-Snapshot::SnapshotChunk::SnapshotChunk()
-  : p_occ_(0)
-  , min_p_occ_(255)
-  , max_p_occ_(0)
-{}
-
-
-Snapshot::SnapshotChunk::SnapshotChunk(const MapChunk &ch, const OccupancyMap &map, MapCache &map_cache)
-  : SnapshotChunk(
-      ch.region.coord, map, map_cache,
-      glm::u8vec3(std::min(ch.first_valid_index.x, uint8_t(map.regionVoxelDimensions().x - 1)),
-                  std::min(ch.first_valid_index.y, uint8_t(map.regionVoxelDimensions().y - 1)),
-                  std::min(ch.first_valid_index.z, uint8_t(map.regionVoxelDimensions().z - 1))),
-      glm::u8vec3(map.regionVoxelDimensions().x, map.regionVoxelDimensions().y, map.regionVoxelDimensions().z))
-{}
-
-
-Snapshot::SnapshotChunk::SnapshotChunk(const glm::i16vec3 &region_key, const OccupancyMap &map, MapCache &map_cache,
-                                       glm::u8vec3 min_ext, glm::u8vec3 max_ext)
-  : p_occ_(0)
-  , min_p_occ_(255)
-  , max_p_occ_(0)
-{
-  // Can end up with empty blocks due to non-power of two or non-uniform chunk sizes
-  if (min_ext.x >= max_ext.x || min_ext.y >= max_ext.y || min_ext.z >= max_ext.z)
-  {
-    // accept default values, representing invalid
-  }
-  else
-  {
-    if (min_ext.x + 1 == max_ext.x && min_ext.y + 1 == max_ext.y && min_ext.z + 1 == max_ext.z)
-    {
-      // Leaf node
-      const Key key(region_key, min_ext);
-      const VoxelConst voxel = map.voxel(key, &map_cache);
-      const bool is_valid = voxel.isValid();
-      const double l_occ = is_valid ? voxel.occupancy() : 0.0;
-      const double exp_l_occ = std::isnan(l_occ) ? 1.0 : std::exp(l_occ);
-      const double p_occ = exp_l_occ / (1.0 + exp_l_occ);
-      p_occ_ = min_p_occ_ = max_p_occ_ = std::isnan(p_occ) ? uint8_t(0) : uint8_t(255*p_occ);
-    }
-    else
-    {
-      // Inner node
-      children_ = std::make_unique<std::array<SnapshotChunk, 8>>();
-      glm::u8vec3 new_min_ext, new_max_ext;
-      unsigned ch_index = 0, num_valid_ch = 0;
-      unsigned p_occ = 0;
-      for (uint8_t x = 0; x < 2; ++x)
-      {
-        new_min_ext.x = x == 0 ? min_ext.x : (min_ext.x + max_ext.x) >> 1;
-        new_max_ext.x = x == 0 ? (min_ext.x + max_ext.x) >> 1 : max_ext.x;
-        for (uint8_t y = 0; y < 2; ++y)
-        {
-          new_min_ext.y = y == 0 ? min_ext.y : (min_ext.y + max_ext.y) >> 1;
-          new_max_ext.y = y == 0 ? (min_ext.y + max_ext.y) >> 1 : max_ext.y;
-          for (uint8_t z = 0; z < 2; ++z)
-          {
-            new_min_ext.z = z == 0 ? min_ext.z : (min_ext.z + max_ext.z) >> 1;
-            new_max_ext.z = z == 0 ? (min_ext.z + max_ext.z) >> 1 : max_ext.z;
-
-            auto &ch = (*children_)[ch_index++] = SnapshotChunk(region_key, map, map_cache, new_min_ext, new_max_ext);
-            if (ch.max_p_occ_ >= ch.min_p_occ_)
-            {
-              p_occ += ch.p_occ_;
-              min_p_occ_ = std::min(min_p_occ_, ch.min_p_occ_);
-              max_p_occ_ = std::max(max_p_occ_, ch.max_p_occ_);
-              ++num_valid_ch;
-            }
-          }
-        }
-      }
-      p_occ_ = uint8_t(p_occ / num_valid_ch); // true mean only for power of two dimensions
-    }
-  }
-}
-
-
-Snapshot::SnapshotChunk::~SnapshotChunk() = default;
-
-
-void Snapshot::SnapshotChunk::simplify(float voxel_size)
-{
-  if (!children_ || min_p_occ_ > max_p_occ_)
-  {
-    return;
-  }
-  if ((max_p_occ_ < 128 && voxel_size < 2.0) ||  //
-      (max_p_occ_ < 64 && voxel_size < 4.0) ||   //
-      (max_p_occ_ < 32) ||                       // any size
-      (max_p_occ_ - min_p_occ_ < 16) ||          // any size
-      (min_p_occ_ >= 192 && voxel_size < 0.25) || //
-      (min_p_occ_ >= 224 && voxel_size < 1))
-  {
-    delete children_.release();
-  }
-  else
-  {
-    for (auto &ch : *children_)
-    {
-      ch.simplify(voxel_size / 2.0f);
-    }
-  }
-}
-
-
-void Snapshot::SnapshotChunk::getVoxels(size_t layer, float voxel_size, glm::f32vec3 centre,
-                                        std::vector<std::pair<uint8_t, glm::f32vec3>> &voxels) const
-{
-  if (layer == 0 && min_p_occ_ <= max_p_occ_)
-  {
-    // we're outputting this layer
-    voxels.emplace_back(p_occ_, centre);
-  }
-  else if (children_)
-  {
-    unsigned ch_index = 0;
-    float new_voxel_size = 0.5*voxel_size;
-    for (float dx = -0.5; dx < 1; dx += 1)
-    {
-      for (float dy = -0.5; dy < 1; dy += 1)
-      {
-        for (float dz = -0.5; dz < 1; dz += 1)
-        {
-          (*children_)[ch_index++].getVoxels(
-            layer - 1, new_voxel_size,
-            glm::f32vec3(centre.x + dx * new_voxel_size, centre.y + dy * new_voxel_size,
-                         centre.z + dz * new_voxel_size), voxels);
-        }
-      }
-    }
-  }
-}
-
-
-size_t Snapshot::SnapshotChunk::treeHeight() const
-{
-  size_t tree_height = 0;
-  if (children_)
-  {
-    for (const auto &ch : *children_)
-    {
-      tree_height = std::max(tree_height, ch.treeHeight());
-    }
-  }
-  return tree_height + 1;
-}
-
-
-size_t Snapshot::SnapshotChunk::memoryUse() const
-{
-  size_t mem_use = sizeof(SnapshotChunk);
-  if (children_)
-  {
-    for (const auto &ch : *children_)
-    {
-      mem_use += ch.memoryUse();
-    }
-  }
-  return mem_use;
-}
-
-
-Snapshot::SnapshotChunk &Snapshot::SnapshotChunk::operator=(SnapshotChunk &&s) = default;
-
-
-
 Snapshot::Snapshot()
-  : imp_(new SnapshotDetail{nullptr})
-{
-}
+  : imp_(new SnapshotDetail{ nullptr })
+{}
 
 
 Snapshot::~Snapshot() = default;
 
 
-std::vector<std::pair<float, std::vector<std::pair<uint8_t, glm::f32vec3>>>> Snapshot::buildSnapshot(const ohm::Aabb &cull_to)
+std::vector<SnapshotRegion> Snapshot::buildSnapshot(const ohm::Aabb &cull_to)
 {
+  std::vector<SnapshotRegion> regions;
+
   if (!imp_->occupancy_map)
   {
-    return std::vector<std::pair<float, std::vector<std::pair<uint8_t, glm::f32vec3>>>>();
+    return regions;
   }
 
   PROFILE(buildSnapshot);
@@ -448,17 +355,6 @@ std::vector<std::pair<float, std::vector<std::pair<uint8_t, glm::f32vec3>>>> Sna
   src_map.enumerateRegions(chunks);
   MapCache map_cache;
 
-  // Construct mean occupancy of each chunk
-  std::vector<std::pair<float, std::vector<std::pair<uint8_t, glm::f32vec3>>>> voxel_layers;
-  const float chunk_size = float(src_map.regionSpatialResolution().x), voxel_size = float(src_map.resolution());
-  float layer_size = chunk_size;
-  while (layer_size >= voxel_size)
-  {
-    voxel_layers.emplace_back(layer_size, std::vector<std::pair<uint8_t, glm::f32vec3>>());
-    layer_size *= 0.5f;
-  }
-
-  size_t mem_use = 0;
   for (const auto ch_ptr : chunks)
   {
     const MapChunk &ch = *ch_ptr;
@@ -467,45 +363,16 @@ std::vector<std::pair<float, std::vector<std::pair<uint8_t, glm::f32vec3>>>> Sna
       continue;
     }
 
-    /*// Calculate mean occupancy within region
-    double mean_occ = 0.0f;
-    size_t num_voxel = 0;
-
-    // firstKeyForChunk
-    const auto region_voxel_dimensions = src_map.regionVoxelDimensions();
-    auto key = Key(ch.region.coord, std::min(ch.first_valid_index.x, uint8_t(region_voxel_dimensions.x - 1)),
-                   std::min(ch.first_valid_index.y, uint8_t(region_voxel_dimensions.y - 1)),
-                   std::min(ch.first_valid_index.z, uint8_t(region_voxel_dimensions.z - 1)));
-    do
-    {
-      const VoxelConst voxel = src_map.voxel(key, &map_cache);
-      const bool is_valid = voxel.isValid();
-      ++num_voxel;
-      const double exp_val = is_valid ? std::exp(voxel.occupancy()) : 1.0;
-      const double occ_prob = exp_val / (1.0 + exp_val);
-      mean_occ += std::isnan(occ_prob) ? 0.5 : occ_prob;
-    } while (nextLocalKey(key, src_map.regionVoxelDimensions()));
-    mean_occupancies.emplace_back(ch.region.centre, num_voxel > 0 ? double(mean_occ / double(num_voxel)) : 0.5f); */
-
-    //SnapshotChunk ss_ch(ch, src_map, map_cache);
-    SnapshotNode ss_ch(ch, src_map, map_cache);
-    ss_ch.simplify(src_map.regionSpatialResolution().x);
-    //mem_use += ss_ch.memoryUse();
-    mem_use += ss_ch.numNode();
-    glm::f32vec3 ch_centre(ch.region.centre);
-    for (size_t i = 0; i < voxel_layers.size(); ++i)
-    {
-      ss_ch.getVoxels(i, chunk_size, ch_centre, voxel_layers[i].second);
-    }
+    SnapshotNode ss_node(ch, src_map, map_cache);
+    ss_node.simplify(src_map.regionSpatialResolution().x);
+    regions.push_back(ss_node.getRegion(0.0, ch.region.coord));
   }
-  std::cout << "snapshot memory: " << mem_use/4/1024 << " kb" << std::endl;
-
 
 #if PROFILING
   ohm::Profile::instance().report();
 #endif  // PROFILING
 
-  return voxel_layers;
+  return regions;
 }
 
 
--- ohm/Snapshot.h
+++ ohm/Snapshot.h
@@ -31,6 +31,13 @@ namespace ohm
   /// Extract a simplified snapshot of a 3D occupancy map.
   ///
 
+  struct SnapshotRegion
+  {
+    double time;
+    glm::i16vec3 region_key;
+    std::vector<uint8_t> data;
+  };
+
   class Snapshot
   {
   public:
@@ -44,55 +51,53 @@ namespace ohm
 
     struct SnapshotNode
     {
+    public:
       SnapshotNode();
+      // Construct tree for given map chunk
       SnapshotNode(const MapChunk &ch, const OccupancyMap &map, MapCache &map_cache);
-      /// Make a snapshot chunk out of this part of region. Valid indices are min_ext.x .. max_ext.x-1, etc
-      SnapshotNode(const glm::i16vec3 &region_key, const OccupancyMap &map, MapCache &map_cache, glm::u8vec3 min_ext,
-                   glm::u8vec3 max_ext);
+      // Construct tree from SnapshotRegion (recursively, starting from specified byte/bit, initialise to zero)
+      SnapshotNode(const SnapshotRegion &region, size_t &byte_address, uint8_t &bit_address);
+      // Note: copies state, does not copy children (required for vector reserve, should not be used otherwise)
+      SnapshotNode(const SnapshotNode &ss);
       ~SnapshotNode();
+
       // Returns the number of free, occupied and unknown voxels underneath node
       std::tuple<uint16_t, uint16_t, uint16_t> simplify(float voxel_size);
+
       // Layer 0 = root (layer \in (0,..,treeHeight()-1))
       void getVoxels(size_t layer, float voxel_size, glm::f32vec3 centre,
                      std::vector<std::pair<uint8_t, glm::f32vec3>> &voxels) const;
+
       // Tree height ()
       size_t treeHeight() const;
+
       // Return approximate memory use
       size_t memoryUse() const;
+
       // Return total number of nodes (packed memory use is number of nodes divided by four)
       size_t numNode() const;
+
+      // Return snapshot region struct with given time and region key, populating data with map contained in this node
+      SnapshotRegion getRegion(double time, const glm::i16vec3 &region_key) const;
 
       SnapshotNode &operator=(SnapshotNode &&s);
 
     private:
-      State state_;
-      std::unique_ptr<std::array<SnapshotNode, 8>> children_;
-    };
+      /// Recursion for snapshot of part of region. Valid indices are min_ext.x .. max_ext.x-1, etc
+      SnapshotNode(const glm::i16vec3 &region_key, const OccupancyMap &map, MapCache &map_cache, glm::u8vec3 min_ext,
+                   glm::u8vec3 max_ext);
 
-    struct SnapshotChunk
-    {
-      SnapshotChunk();
-      SnapshotChunk(const MapChunk &ch, const OccupancyMap &map, MapCache &map_cache);
-      /// Make a snapshot chunk out of this part of region. Valid indices are min_ext.x .. max_ext.x-1, etc
-      SnapshotChunk(const glm::i16vec3 &region_key, const OccupancyMap &map, MapCache &map_cache, glm::u8vec3 min_ext,
-                    glm::u8vec3 max_ext);
-      ~SnapshotChunk();
-      void simplify(float voxel_size);
-      // Layer 0 = root (layer \in (0,..,treeHeight()-1))
-      void getVoxels(size_t layer, float voxel_size, glm::f32vec3 centre,
-                     std::vector<std::pair<uint8_t, glm::f32vec3>> &voxels) const;
-      // Tree height ()
-      size_t treeHeight() const;
-      // Return approximate memory use
-      size_t memoryUse() const;
-
-      SnapshotChunk &operator=(SnapshotChunk &&s);
+      // Recursion for populating SnapshotRegion with data beneath this node in tree
+      // bits_used_in_final_byte contains number of bits used so far in final byte of region.data (if zero, byte has not
+      // yet been allocated). Note that first node is represented in LSB.
+      void getRegion(SnapshotRegion &region, uint8_t &bits_used_in_final_byte) const;
 
     private:
-      uint8_t p_occ_, min_p_occ_, max_p_occ_; // node empty/invalid if min_p_occ_ > max_p_occ_
-      std::unique_ptr<std::array<SnapshotChunk, 8>> children_;
+      State state_;
+      std::unique_ptr<std::array<SnapshotNode, 8>> children_;
+      static constexpr uint8_t kStateMask = 0x3; // mask to current node off byte
+      static constexpr uint8_t kStateBits = 2; // number of bits (must have even number of these in a byte)
     };
-
 
     /// Construct a default initialised snapshot object.
     Snapshot();
@@ -104,8 +109,7 @@ namespace ohm
     ///
     /// @param cull_to The box (xy) to restrict snapshot to.
     /// @return a vector of chunk centres and mean occupancy.
-    std::vector<std::pair<float, std::vector<std::pair<uint8_t, glm::f32vec3>>>> buildSnapshot(
-      const ohm::Aabb &cull_to = ohm::Aabb(0.0));
+    std::vector<SnapshotRegion> buildSnapshot(const ohm::Aabb &cull_to = ohm::Aabb(0.0));
 
     /// Set the (source) occupancy map. Does not take ownership of the pointer so the @p map must persist until 
     /// @c buildSnapshot() is called.
