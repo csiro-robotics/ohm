ae082e87db80dba192c9f137bf301bec7423f963
Kazys Stepanas <kazys.stepanas@csiro.au>
2022-11-11 15:18:01 +1000

    Fix test file resource args generation

--- cmake/TextFileResource.py
+++ cmake/TextFileResource.py
@@ -3,7 +3,6 @@
 This can be used to embed a text file into a compiled binary.
 """
 import argparse
-import binascii
 import copy
 import os.path
 import re
@@ -12,10 +11,12 @@ import sys
 if sys.version_info[0] == 2:
     import __future__
 
+
 #-------------------------------------------------------------------------------
 # Argument parsing setup
 #-------------------------------------------------------------------------------
 class ArgParser(argparse.ArgumentParser):
+
     def error(self, message):
         if len(sys.argv) > 1:
             sys.stderr.write('error: %s\n' % message)
@@ -25,8 +26,14 @@ class ArgParser(argparse.ArgumentParser):
             self.print_help()
         sys.exit(0)
 
+
 class StyleInfo:
-    def __init__(self, file_matches = (), comment_regex = None, include_regex = None, priority = 0):
+
+    def __init__(self,
+                 file_matches=(),
+                 comment_regex=None,
+                 include_regex=None,
+                 priority=0):
         self.extensions = file_matches
         self.comment_regex = comment_regex
         self.include_regex = include_regex
@@ -35,20 +42,34 @@ class StyleInfo:
 
 def buildStyles():
     # Base C-Style comment
-    c_style_comment = re.compile(r'(^[ \t\r]*\n)|(^[ \t]*//.*\n)|(//.*$)|(/\*.*\*/)', re.M)
+    c_style_comment = re.compile(
+        r'(^[ \t\r]*\n)|(^[ \t]*//.*\n)|(//.*$)|(/\*.*\*/)', re.M)
     # C/C++ include regular expression
     c_style_include = re.compile(r'^[ \t]*#include[ \t]+[<"](.*)[>"].*$', re.M)
     styles = {
-        'none': StyleInfo(),
-        'text': StyleInfo(),
-        'c': StyleInfo(['c', 'h'], c_style_comment, c_style_include),
-        'c#': StyleInfo(['cs'], c_style_comment),
-        'cmake': StyleInfo(['cmake', 'CMakeLists.txt'], re.compile(r'#.*$', re.M)),
-        'cpp': StyleInfo(['cpp', 'cxx', 'h', 'hpp', 'hxx'], c_style_comment, c_style_include, priority = 1),
-        'cuda': StyleInfo(['cu'], c_style_comment, c_style_include),
-        'java': StyleInfo(['java'], c_style_comment),
-        'opencl': StyleInfo(['cl'], c_style_comment, c_style_include),
-        'python': StyleInfo(['py'], c_style_comment)
+        'none':
+        StyleInfo(),
+        'text':
+        StyleInfo(),
+        'c':
+        StyleInfo(['c', 'h'], c_style_comment, c_style_include),
+        'c#':
+        StyleInfo(['cs'], c_style_comment),
+        'cmake':
+        StyleInfo(['cmake', 'CMakeLists.txt'], re.compile(r'#.*$', re.M)),
+        'cpp':
+        StyleInfo(['cpp', 'cxx', 'h', 'hpp', 'hxx'],
+                  c_style_comment,
+                  c_style_include,
+                  priority=1),
+        'cuda':
+        StyleInfo(['cu'], c_style_comment, c_style_include),
+        'java':
+        StyleInfo(['java'], c_style_comment),
+        'opencl':
+        StyleInfo(['cl'], c_style_comment, c_style_include),
+        'python':
+        StyleInfo(['py'], c_style_comment)
     }
     return styles
 
@@ -61,51 +82,94 @@ def str2bool(val):
     else:
         raise argparse.ArgumentTypeError('Invalid boolean string: %s' % val)
 
+
 parser = ArgParser(description = \
 """Convert a text file into a symbol into a symbol contained in a source file.
 
 This can be used to embed a text file into a compiled binary.
 """)
 
-
 # Build styles
 text_file_styles = buildStyles()
 
-parser.add_argument('input', metavar = 'input', type = str,
-                   help = 'Text file to convert into a C++ compilation unit.')
-parser.add_argument('output', metavar = 'output', type = str,
-                   help = 'Base name/path for the output without extension.')
+parser.add_argument('input',
+                    metavar='input',
+                    type=str,
+                    help='Text file to convert into a C++ compilation unit.')
+parser.add_argument('output',
+                    metavar='output',
+                    type=str,
+                    help='Base name/path for the output without extension.')
 
-parser.add_argument('--src-ext', type = str, default = 'cpp',
-                   help = 'File extention for the source file. Default is cpp.')
-parser.add_argument('--h-ext', type = str, default = 'h',
-                   help = 'File extention for the header file.')
-parser.add_argument('--mode', type = str, default = 'text', choices = ('binary', 'text'),
-                    help = 'Output mode control; text mode writes as readable text, while bytes write as an array of ' +
-                            'bytes. Bytes are also written if the string content is too long.')
-parser.add_argument('--style', type = str, default = 'none', choices = text_file_styles.keys(),
-                    help = 'Explicitly specify the file type. Affects comment and include directive handling. ' +
-                            'Matched by extension when set to none. Use "text" for no style.')
-parser.add_argument('--comment-regex', type = str,
-                    help = 'Override for the regular expression used to match comments for minification.' +
-                            'Applied as a multi-line regular expression.')
-parser.add_argument("--name", type = str,
-                    help = "Specifies the output variable name. The default is '<filename>Resource' without the path.")
-parser.add_argument('--minify', type = str, default = 'off', choices = ('off', 'on', 'comments', 'whitespace'),
-                    help = 'Minification mode.\n' +
-                            '\t\toff : leave as is.\n' +
-                            '\t\ton  : full minification.\n' +
-                            '\t\tcomments : strip comment strings. See also --comment-style.\n' +
-                            '\t\twhitespace : strip leading and trailing whitespace.')
-parser.add_argument('--no-import', type = str2bool, const = True, nargs='?',
-                    help = 'Do not expand include/import statements?')
-parser.add_argument('--include-regex', type = str,
-                    help = 'Override for the regular expression used to "include" other files. Expression is ' +
-                            'replaced by the file identified in capture 1. This file is searched on the -I paths')
-parser.add_argument('-I', type = str, dest = 'include_dirs', action='append',
-                    help = 'Specifies a path used to resolve include statements. May be specified multiple times.')
+parser.add_argument('--src-ext',
+                    type=str,
+                    default='cpp',
+                    help='File extention for the source file. Default is cpp.')
+parser.add_argument('--h-ext',
+                    type=str,
+                    default='h',
+                    help='File extention for the header file.')
+parser.add_argument(
+    '--mode',
+    type=str,
+    default='text',
+    choices=('binary', 'text'),
+    help=
+    'Output mode control; text mode writes as readable text, while bytes write as an array of '
+    + 'bytes. Bytes are also written if the string content is too long.')
+parser.add_argument(
+    '--style',
+    type=str,
+    default='none',
+    choices=text_file_styles.keys(),
+    help=
+    'Explicitly specify the file type. Affects comment and include directive handling. '
+    + 'Matched by extension when set to none. Use "text" for no style.')
+parser.add_argument(
+    '--comment-regex',
+    type=str,
+    help=
+    'Override for the regular expression used to match comments for minification.'
+    + 'Applied as a multi-line regular expression.')
+parser.add_argument(
+    "--name",
+    type=str,
+    help=
+    "Specifies the output variable name. The default is '<filename>Resource' without the path."
+)
+parser.add_argument(
+    '--minify',
+    type=str,
+    default='off',
+    choices=('off', 'on', 'comments', 'whitespace'),
+    help='Minification mode.\n' + '\t\toff : leave as is.\n' +
+    '\t\ton  : full minification.\n' +
+    '\t\tcomments : strip comment strings. See also --comment-style.\n' +
+    '\t\twhitespace : strip leading and trailing whitespace.')
+parser.add_argument('--no-import',
+                    type=str2bool,
+                    const=True,
+                    nargs='?',
+                    help='Do not expand include/import statements?')
+parser.add_argument(
+    '--include-regex',
+    type=str,
+    help=
+    'Override for the regular expression used to "include" other files. Expression is '
+    +
+    'replaced by the file identified in capture 1. This file is searched on the -I paths'
+)
+parser.add_argument(
+    '-I',
+    type=str,
+    dest='include_dirs',
+    action='append',
+    help=
+    'Specifies a path used to resolve include statements. May be specified multiple times.'
+)
 
 args = parser.parse_args()
+
 
 #-------------------------------------------------------------------------------
 # Other functions
@@ -118,12 +182,14 @@ def resolveIncludeFilePath(include_file, include_dirs):
             return path
     return None
 
+
 def resolveIncludeDirectives(regex, content, include_dirs):
     directive = regex.search(content)
     haveShownPaths = False
     included_file_paths = []
     while directive:
-        include_file_path = resolveIncludeFilePath(directive.group(1), include_dirs)
+        include_file_path = resolveIncludeFilePath(directive.group(1),
+                                                   include_dirs)
         next_search_offset = directive.start()
         if include_file_path:
             # Load replacement text if we haven't already
@@ -132,16 +198,19 @@ def resolveIncludeDirectives(regex, content, include_dirs):
             with open(include_file_path, 'r') as include_file:
                 include_content = include_file.read()
             included_file_paths.append(include_file_path)
-            content = content[:directive.start()] + include_content + content[directive.end():]
+            content = content[:directive.start(
+            )] + include_content + content[directive.end():]
         else:
             # Failed to resolve.
             if not haveShownPaths:
                 haveShownPaths = True
                 print("Using include paths:", include_dirs)
-            print("Failed to resolve include", directive.group(0), ":", directive.group(1))
+            print("Failed to resolve include", directive.group(0), ":",
+                  directive.group(1))
             next_search_offset = directive.end()
         directive = regex.search(content, next_search_offset)
     return content
+
 
 def selectStyle(styles, args):
     if args.style == 'none':
@@ -228,7 +297,9 @@ with open(args.input, 'r') as input_file:
 
 # Find include directives
 if style.include_regex and not args.no_import:
-    input_file_content = resolveIncludeDirectives(style.include_regex, input_file_content, args.include_dirs)
+    input_file_content = resolveIncludeDirectives(style.include_regex,
+                                                  input_file_content,
+                                                  args.include_dirs)
 
 # Minify if required
 minify_whitepace = args.minify == 'whitespace'
@@ -242,9 +313,12 @@ if minify_comments and style.comment_regex != None:
     input_file_content = style.comment_regex.sub('', input_file_content)
 if minify_whitepace:
     # Remove trailing whitespace
-    input_file_content = re.sub(r'[ \t]+$', '', input_file_content, flags = re.M)
+    input_file_content = re.sub(r'[ \t]+$', '', input_file_content, flags=re.M)
     # Replace leading whitespace
-    input_file_content = re.sub(r'(^[ \t]+)', '', input_file_content, flags = re.M)
+    input_file_content = re.sub(r'(^[ \t]+)',
+                                '',
+                                input_file_content,
+                                flags=re.M)
 
 input_file_content_length = len(input_file_content)
 
@@ -259,8 +333,10 @@ if input_file_content_length < 1 << 16 and args.mode == 'text':
     # Replace quotes.
     input_file_content_string = input_file_content.replace('"', '\\"')
     # Replace backslashes excluding the quoted we just escaped.
-    input_file_content_string = re.sub(r'\\(?!")', r'\\\\', input_file_content_string)
-    input_file_content_string = re.sub(r'\r?\n', r'\\n"\n"', input_file_content_string)
+    input_file_content_string = re.sub(r'\\(?!")', r'\\\\',
+                                       input_file_content_string)
+    input_file_content_string = re.sub(r'\r?\n', r'\\n"\n"',
+                                       input_file_content_string)
     input_file_content_string = re.sub(r'\t', '\\t', input_file_content_string)
     input_file_content_string = '"' + input_file_content_string + '"'
 else:
@@ -288,9 +364,12 @@ with open(args.output_source, 'w') as source_file:
 #include "{1}"
 
 const unsigned {2}_length = {3};  // NOLINT
-static const char {2}_[] =  // NOLINT\n""".format(args.input, os.path.basename(args.output_header), resource_name, input_file_content_length))
+static const char {2}_[] =  // NOLINT\n""".format(
+        args.input, os.path.basename(args.output_header), resource_name,
+        input_file_content_length))
 
     source_file.write(input_file_content_string)
     source_file.write('; // NOLINT\n')
-    source_file.write("const char * const {0} = {0}_; // NOLINT\n".format(resource_name))
+    source_file.write(
+        "const char * const {0} = {0}_; // NOLINT\n".format(resource_name))
     source_file.write('\n')
